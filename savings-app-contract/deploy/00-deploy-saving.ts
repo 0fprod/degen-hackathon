5db29445edc49ac9f415a1be99a4abeb4358bea0
0fprod <franp.dev@protonmail.com>
2024-02-17 15:28:47 +0000

    config: deploy to hh local

--- savings-app-contract/deploy/00-deploy-saving.ts
+++ savings-app-contract/deploy/00-deploy-saving.ts
@@ -2,16 +2,20 @@ import { run, ethers } from "hardhat";
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 
 const deploy = async (hre: HardhatRuntimeEnvironment) => {
-  const { log } = hre.deployments;
+  const { log, } = hre.deployments;
+  const chainId = await hre.getChainId()
+  const hardhatChainId = '31337';
   log('Deploying Saving...');
   const savings = await ethers.deployContract("Saving");
   const address = await savings.getAddress();
   log(`Saving deployed at ${address}`);
 
-  await run('verify:verify', {
-    address,
-    constructorArguments: [],
-  });
+  if (chainId !== hardhatChainId) {
+    await run('verify:verify', {
+      address,
+      constructorArguments: [],
+    });
+  }
 };
 
 export default deploy;
